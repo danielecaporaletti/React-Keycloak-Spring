ad99b50c7e3d8361569faae076e949c6719f18c2
danielecaporaletti <danielecaporaletti1@gmail.com>
2023-05-27 07:55:23 +0200

    change ip

--- Front-End-React/src/config.js
+++ Front-End-React/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL_FRONTEND = 'http://34.76.102.17:80';
-export const BASE_URL_BACKEND = 'http://34.76.102.17:8090';
-export const BASE_URL_KEYCLOAK = 'http://34.76.102.17:8080';
+export const BASE_URL_FRONTEND = 'http://localhost:80';
+export const BASE_URL_BACKEND = 'http://localhost:8090';
+export const BASE_URL_KEYCLOAK = 'http://localhost:8080';
